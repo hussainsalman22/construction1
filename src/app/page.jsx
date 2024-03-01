b89a8b3c9241bf946e2aba2e14f4478b395442e3
hussainsalman22 <hussainqureshi22jan@gmail.com>
2024-03-01 20:28:29 +0500

    new2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div id="Home">
     <ChakraProvider>
-    <Navbar home="#home" service="#service" contact="#contact" jobs="#" />
+    {/* <Navbar home="#home" service="#service" contact="#contact" jobs="#" /> */}
     <Hero/>
     <Service/>
     <Contactus/>
