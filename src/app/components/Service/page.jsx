a6353ad08c6ca1c6733207c8b556f862e5255653
hussainsalman22 <hussainqureshi22jan@gmail.com>
2024-03-24 17:06:23 +0500

    change2

--- src/app/components/Service/page.jsx
+++ src/app/components/Service/page.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-// import LargedevicesService  from "../LargedevicesService/page"
+import LargedevicesService  from "../../components/LargedevicesService/page.jsx"
 import MobileService from "../Mobileservices/page.jsx"
 
 const Service = () => {
     return (
         <div id='service'>
             <div className="md:block hidden">
-                {/* < LargedevicesService /> */}
+                < LargedevicesService />
             </div>
             <div className="md:hidden block">
                 <MobileService />
